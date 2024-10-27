56c9ebf26d16a7040cd5dc18974f02b53410fcc7
Ayush kumar <kumarayush042004@gmail.com>
2024-10-27 21:24:05 +0530

    updated file

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
- document.querySelectorAll('a[href^="#"]').forEach( anchor =>{
+    const  anchors = document.querySelectorAll('a[href^="#"]')
+    anchors.forEach( anchor =>{
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
         const target = document.querySelector(this.getAttribute('href'));
         if(target){
             window.scrollTo({
-                top: target.offsetTop,
-                behavior: 'smooth'
+                top: target.offsetTop, 
             });
         }
     });
